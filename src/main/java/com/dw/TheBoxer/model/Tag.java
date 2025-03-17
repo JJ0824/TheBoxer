6debded0aae369cd9aaa583d888a97554fd721c5
JJ0824 <auxjungjae@gmail.com>
2025-03-17 23:19:54 +0900

    update

--- src/main/java/com/dw/TheBoxer/model/Tag.java
+++ src/main/java/com/dw/TheBoxer/model/Tag.java
@@ -3,7 +3,6 @@ package com.dw.TheBoxer.model;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor
@@ -15,9 +14,9 @@ import java.util.List;
 @Table(name = "tag")
 public class Tag {
     @Id
-    private String name;  // 태그 이름 (예: '복싱화', '샌드백', '루틴', '식단' 등 관리자가  고정)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-
-    @OneToMany(mappedBy = "tag")  // 하나의 태그는 여러 게시글을 가질 수 있음
-    private List<Board> boards = new ArrayList<>();  // 해당 태그가 포함된 게시글 목록
+    @ManyToMany(mappedBy = "tag")
+    private List<Board> boards;  // 해당 태그가 포함된 게시글 목록
 }
